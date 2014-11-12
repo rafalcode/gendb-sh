ec61a50d047e7f5beeb791a15162f5f7844721d7
vt <hdbuster@gmail.com>
2014-11-12 17:42:33 +0000

    rmeove config

--- config.py
+++
@@ -1,9 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vt3:J39s9QD:@vt3.mysql.cs.st-andrews.ac.uk/vt3_db'
-SECRET_KEY = ''
-WTF_CSRF_ENABLED = True
-SECRET_KEY =  '432njrjh245hjfds&A2hkj4'
-
-UPLOAD_FOLDER = '/cs/home/vt3/SHproject/uploads'
-ALLOWED_EXTENSIONS = set(['ped', 'map', 'csv'])
-
-
