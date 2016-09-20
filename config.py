c9c217643475690c585bfde6feba72bc873d61ae
rafalcode <ramonfallon@gmail.com>
2016-09-20 17:31:00 +0200

    cleaning

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vt3:J39s9QD:@vt3.mysql.cs.st-andrews.ac.uk/vt3_db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vt3:20Vt316:@localhost/vt3_db'
 SECRET_KEY = ''
 WTF_CSRF_ENABLED = True
-UPLOAD_FOLDER = '/cs/home/vt3/SHproject/uploads
+UPLOAD_FOLDER = '/var/www/gendbdemo/uploads'
 ALLOWED_EXTENSIONS = set(['ped', 'map', 'csv'])
